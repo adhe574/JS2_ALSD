a3e0af1859f23dde9d5a6bd62b2f9d6881d462a9
adhe574 <adhewidya378@gmail.com>
2025-02-20 21:25:06 +0700

    simpan

--- MahasiswaMain01.java
+++ MahasiswaMain01.java
@@ -2,6 +2,7 @@ public class MahasiswaMain01 {
     public static void main(String[] args) {
             
             Mahasiswa01 mhs1 = new Mahasiswa01();
+
             mhs1.nama = "Muhammad Ali Farhan";
             mhs1.nim = "2241720171";
             mhs1.kelas = "SI 2J";
@@ -15,5 +16,8 @@ public class MahasiswaMain01 {
             Mahasiswa01 mhs2 = new Mahasiswa01("Annisa Nabila", "2141720160", "TI 2L", 3.25);
             mhs2.updateIpk(3.30);
             mhs2.tampilkanInformasi();
+
+            Mahasiswa01 mhsAdhe = new Mahasiswa01("Adhe Widya", "244107060067", "SIB 1C", 3.60);
+            mhsAdhe.tampilkanInformasi();
     }  
 }
